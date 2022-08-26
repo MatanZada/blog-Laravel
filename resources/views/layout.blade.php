cc270c636dc1ce8108c2ca1b6237185277e32f57
matan <you@example.com>
2022-08-26 17:02:02 +0300

    Displays success messages in green and writes, and error messages and writes the error in red,

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
-        @yield('title','Blog')
+        @yield('title', 'Blog')
     </title>
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
+
     <ul class="nav">
         <li><a class="{{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a></li>
         <li><a class="{{ request()->routeIs('about') ? 'active' : '' }}" href="{{ route('about') }}">About</a></li>
+        <li><a class="{{ request()->routeIs('posts.create') ? 'active' : '' }}" href="{{ route('posts.create') }}">Create Post</a></li>
     </ul>
+
+    @includeWhen($errors->any(), '_errors')
+
+    @if (session('success'))
+    <div class="flash-success">
+        {{ session('success') }}
+    </div>
+    @endif
 
     <div class="main">
         @yield('content')
