de02e22781a656338772bcc973195ffd70ccd7c0
matan <you@example.com>
2022-08-28 23:30:34 +0300

    verification application, registration, login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,10 @@ Route::get('/about', [HomeController::class, 'about'])->name('about');
 
 Route::resource('posts', PostController::class)->except([
     'index',
-]);
+])->middleware('auth');
+
+Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register')->middleware('guest');
+
+Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
